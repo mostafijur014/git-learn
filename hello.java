7b87fa0ec5b0646b44baf88a70e1c1ba2220a5d1
MR Mostafijur <mostafijur014@gmail.com>
2021-04-14 02:52:45 +0600

    We are added a new method called display

--- hello.java
+++ hello.java
@@ -1,5 +1,11 @@
 public class hello{
     public static void main (String[] args){
         System.out.println("Vs code is awesome");
+        greeting("MR Mostafijur");
     }
+
+    public static void greeting(String name){
+        System.out.println("Welcome" +name);
+    }
+
 }
